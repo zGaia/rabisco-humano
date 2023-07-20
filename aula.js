612a4d58879f6921b79383b7739f66dd564b2bf9
Silas <139024636+zGaia@users.noreply.github.com>
2023-07-20 12:15:51 +0000

    primeiro comit

---
+++ aula.js
@@ -0,0 +1,18 @@
+var minhaVar
+minhaVar = 10
+var soma
+soma = 15 + 8
+
+soma += 1
+soma *= 3
+console.log(soma)
+
+var fazSol = true 
+var comida = ['arroz', 'feijão','ovo']
+console.log(comida[1])
+
+if (soma === minhaVar) {
+    console.log("é igual:");
+  } else {
+    console.log("não é igual:")
+  }
