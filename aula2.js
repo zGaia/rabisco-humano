641f0710e583415b2f4cc3e17b326b7f00de6e39
Silas <139024636+zGaia@users.noreply.github.com>
2023-07-25 13:17:13 +0000

    aula2.js

--- aula2.js
+++ aula2.js
@@ -1,5 +1,4 @@
-let idade = 18
-
+/*let idade = 18
 
 if (idade < 16 ) {
     console.log("Não pode dirigir")
@@ -8,3 +7,136 @@ if (idade < 16 ) {
   } else {
     console.log("Pode dirigir e tirar habilitação")
   }
+*/
+/*
+let diaDaSemana = 8
+
+switch (diaDaSemana) {
+    case 1:
+        console.log("Domingo");
+        break;
+    case 2:
+        console.log("Segunda-feira");
+        break;
+    case 3:
+        console.log("Terça-Feira");
+        break;
+    case 4:
+        console.log("Quarta-Feira");
+        break;
+    case 5:
+        console.log("Quinta-Feira");
+        break;
+    case 6:
+        console.log("Sexta-Feira")
+        break;
+    case 7:
+        console.log("Sábado");
+        break;
+
+    default: console.log("Tente novamente.")
+        break;
+}
+*/
+/*
+let mes =  12
+
+switch (mes) {
+    case 1:
+        console.log("Janeirio");        
+        break;
+    case 2:
+        console.log("Fevcereiro");        
+        break;
+    case 3:
+        console.log("Março");        
+        break;
+    case 4:
+        console.log("Abriu");        
+        break;
+    case 5:
+        console.log("Maio");        
+        break;
+    case 6:
+        console.log("Julho");        
+        break;
+    case 7:
+        console.log("Julho");        
+        break;
+    case 8:
+        console.log("Agosto");        
+        break;
+    case 9:
+        console.log("Setembro");    
+        break;
+    case 10:
+        console.log("Outubro");
+        break;
+    case 11:
+        console.log("Novembro");
+        break;
+    case 12:
+        console.log("Dezembro")
+        break;
+
+    default: console.log("Tente novamente.")
+        break;
+}
+
+*/
+/*
+let estadoCivil = 'solteiro'
+
+switch (estadoCivil) {
+    case 'solteiro':
+        console.log("Solteiro");
+        break;
+    case 'casado':
+        console.log("Casado");
+        break;
+    case 'divorciado':
+        console.log("Divorciado");
+        break;
+    case 'outro':
+        console.log("Outros");
+        break;        
+    default: console.log("Verifique e tente Novamente")
+        break;
+}
+ */
+/*
+let tipoAnimal = "gato"
+
+switch (tipoAnimal) {
+    case 'gato':
+        console.log("Gato");
+        break;
+    case 'cahcorro':
+        console.log("Cachorro");
+        break;
+    case 'passaro':
+        console.log("Pássaro");
+        break;
+    case 'outro':
+        console.log("Outros");
+        break;        
+    default: console.log("Verifique e tente Novamente")
+        break;
+}
+*/
+/*
+let operador = "+";
+
+switch (operador) {
+    case "+":
+        console.log("Soma");
+        break;
+    case "-":
+        console.log("Diferença")
+        break;
+    case "Não sei":
+        break
+    default:
+        break;
+}
+*/
