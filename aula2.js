b2f01e1f4bde1e3b1fb0de570877600f758a5d39
Silas <139024636+zGaia@users.noreply.github.com>
2023-07-25 12:12:03 +0000

    aula2.js

---
+++ aula2.js
@@ -0,0 +1,10 @@
+let idade = 18
+
+
+if (idade < 16 ) {
+    console.log("Não pode dirigir")
+  } else if (idade < 18) {
+    console.log("Pode dirigir.")
+  } else {
+    console.log("Pode dirigir e tirar habilitação")
+  }
