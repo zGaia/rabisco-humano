197e6228e3728f056eb67ee74b27ddee81b96046
Silas <139024636+zSilas@users.noreply.github.com>
2023-08-03 13:51:28 +0000

    aula3

---
+++ aula3.js
@@ -0,0 +1,80 @@
+/*
+let x = 0
+
+while (x <= 10) {
+    console.log(x);
+    x ++;
+    
+}
+*/
+/*
+num = 1
+let soma = 0
+
+while (num <= 100) {
+   soma += num
+   num ++;
+}
+console.log(soma)
+*/
+/*
+let x = 1
+
+while (x <= 50) {
+    if (x %2 == 0 ){
+        console.log(x)
+    }
+    x ++
+}
+*/
+/*
+let x = 2
+while ( x < 1000) {
+    x *= 2
+}
+    console.log(x)
+*/
+
+/*
+let x = 10
+
+while (x >= 1) {
+    console.log(x);
+    x --;   
+}
+*/
+/*
+let senha
+let senha1 = 123
+let rodando = true
+
+while (rodando) {
+   senha = require('readline-sync').question("Digite a senha.")
+    if (senha == senha1) {
+        console.log("Acesso permitido.")
+        rodando = false
+    } else {}
+        console.log("Senha incorreta")
+}
+*/
+/*
+let x = 3
+    while (x <= 30 ) {
+        console.log (x)
+        x += 3
+    }
+*/
+/*
+let i = 0
+let media 
+let soma = 0 
+let lista = [1,2,3,4,5]
+while (i < lista.length) {
+    (lista[i])
+    soma += lista[i]
+    i++
+}
+        media = soma/lista.length 
+        console.log (media)
+
+*/
