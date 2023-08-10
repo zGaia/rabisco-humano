5bfbaace9e7b0c818e8054e82010971d5628cf67
Silas <139024636+zSilas@users.noreply.github.com>
2023-08-10 14:20:33 +0000

    aula4.js

---
+++ aula4.js
@@ -0,0 +1,95 @@
+/*
+function exibirSaudacao(nome, saudacao='Nome') {
+    console.log(`${nome}, ${saudacao}`);
+}
+exibirSaudacao("Olá")
+*/
+/*
+let num1 = 10
+let num2 = 9
+
+function exibirOperacao(numero1, numero2, operacao= '+') {
+    switch (operacao) {
+        case "+":
+            console.log(`A soma é: ${numero1+numero2}`)
+            break;
+        case "-":
+            console.log(`A subtração é: ${numero1-numero2}`)
+            break;
+        case "/":
+            console.log(`A divisão é: ${numero1/numero2}`)
+            break;
+        case "*":
+            console.log(`A multiplicação é: ${numero1*numero2}`)
+            break;
+        default:
+            break;
+    }
+}
+exibirOperacao (num1, num2,"/")
+
+*/
+/*
+function contagemRegresasiva(contador=10) {
+    while (contador > 0) {
+        console.log(contador)
+            contador --;
+    }   console.log(`Contagem regressiva para o lançamento: ${contador}`)
+}   
+contagemRegresasiva()
+*/
+/*
+function exibirApresentacao(nome, idade, profissao='nome') {
+    console.log(`Olá, eu sou ${nome}, tenho ${idade} e sou ${profissao}`)
+    
+}
+exibirApresentacao("Azul", "16", "Doido")
+*/
+/*
+function mernsagemPersonalizada(texto, formato = normal) {
+ switch (formato) {
+    case "normal":
+        console.log(`Texto neste modo ${formato}`)
+        break;
+    case "negrito":
+        console.log(`Texto neste modo ${formato}`)
+        break;
+    case "italico":
+        console.log(`Texto neste modo ${formato}`)
+        break
+    default:
+        break;
+ }   
+    
+}
+mernsagemPersonalizada("texto", "negrito")
+*/
+/*
+function criarMensagemDeSaudacao(nome) {
+    const mensagem = `Opa consagrado ${nome}`;
+    return mensagem;
+}
+    let a = criarMensagemDeSaudacao("Azebio")
+console.log(a)
+*/
+/*
+let numero1 = 10
+let numero2 = 9
+function somaNumeros(numero1, numero2) {
+    const soma = numero1 + numero2
+    return soma;
+}
+console.log(somaNumeros(numero1,numero2))
+*/
+/*
+let num = 10
+function ehPar(num) {
+ if (num %2 ==0) {
+   return true
+ } else { 
+
+   return false
+ }
+}
+console.log(ehPar(num))
+*/
