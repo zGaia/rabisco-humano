7bbb841d1b3986a642132bbe604043a866b2183b
Silas <139024636+zSilas@users.noreply.github.com>
2023-08-24 14:15:49 +0000

    aula6.js

---
+++ aula6.js
@@ -0,0 +1,149 @@
+// ex
+/*
+const livro = {
+titulo: '1984',
+autor: 'George Orwell',
+isDisponivel: false,
+
+checkin: function () {
+    this.isDisponivel= true;
+},
+checkout: function () {
+    this.isDisponivel= false;
+},
+};
+console.log(livro.checkin)
+*/
+/* 1Q
+const pessoa = {
+};
+
+const pessoa = new Object ();
+pessoa.atributo
+*/
+
+const { questionPath } = require("readline-sync");
+
+/* 2Q
+
+const pessoa = {
+    nome: 'João',
+};
+*/
+
+/* 3Q
+
+const usuario = {
+    idade: 300,
+};
+console.log(usuario.idade)
+*/
+/* 4Q
+
+const produto = {
+    preco: 50
+};
+produto.preco = 75
+*/
+
+/* 5Q
+
+const contato = {
+    nome: 'fulano',
+    telefone: 123456789,
+    email: 'abcd@gmail.com',
+};
+delete contato.email
+*/
+
+/* 6Q
+
+const cliente = {
+    nome: 'Fulano',
+    email: 'abc@gmail.com',
+    tel: 12345678,
+}
+cliente.endereco='Rua A, 123'
+console.log(cliente)
+*/
+
+/* 7Q
+
+const empresa = {
+    departamentos:['bebidas', 'comidas', 'brinquedos']
+}
+console.log(empresa.departamentos[0])
+*/
+
+/* 8Q
+
+const aluno = {
+    notas: [85,90,78]
+}
+aluno.notas[1]=95
+console.log(aluno.notas)
+*/
+
+/* 9Q
+
+const dados = {
+    valores:[10, 20, 30]
+}
+delete dados.valores[1]
+dados.valores.splice[1,1]
+console.log(dados)
+*/
+
+/* 10Q
+
+const livro = {
+    titulo: 'A Morte da cabrita',
+    Autor: 'Maria'
+}
+console.log(livro)
+*/
+/* Q1
+const DIASDASEMANA = {
+    SEGUNDA :'Segunda-Feira',
+    TERCA : 'Terça-Feira',
+    QUARTA : 'Quarta-Feira',
+    QUINTA : 'Quinta- Feira', 
+    SEXTA : 'Sexta-Feira',
+    SABADO : 'Sabado',
+    DOMINGO : 'Domingo',
+}
+*/
+/* Q2
+const CHEFES = {
+    ELITES: 'Soldados',
+    EPICOS :'Generais',
+    SUBBOSS : 'Guardião',
+    BOSS : 'Chefe final',
+}
+*/ 
+/* Q3
+cosnt PASSAGENS = {
+    PASS1: 'Econominca'
+    PASS2: 'Execultiva'
+    PASS3: 'Primeira Classe'
+
+}
+*/
+
+/* Q4
+
+const TAREFAS ={
+    TAREFA1:'Trabalho'
+    TAREFA2:'Estudo'
+    TAREFA3:'Lazer'
+}
+*/
+
+/* Q5
+
+cosnt STATUS ={
+    STS1: 'Em processamento'
+    STS2: 'Enviado',
+    STS3: 'Entregue',
+}
+*/
